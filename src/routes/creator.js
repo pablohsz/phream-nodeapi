b16b61acacf91e5af6eb22c10827938e39ad8e15
pablohsz <pablosouza.dev@gmail.com>
2023-08-03 00:58:54 -0300

    removendo comentários obsoletos

--- src/routes/creator.js
+++ src/routes/creator.js
@@ -3,35 +3,19 @@ const router = express.Router();
 const dao = require('../dao/creatorDao');
 
 
-//Retorna todos os pedidos 
+
 router.get('/', async (req, res, next)=> {
 
-    let busca = await dao.selectCreator();
+    let creators = await dao.selectCreator();
 
     res.status(200).send({
         mensagem: 'Usando o GET dentro da rota de criadores',
-        resultado: busca
+        creators: creators
     });
 });
 
-//Insere um pedido
+
 router.post('/', (req, res, next)=> {
-
-
-
-
-    pg.getConnection((error, conn) =>{
-        conn.query(
-            'INSERT INTO creators (creatorname, creator_pass, creator_nick, creator_email, creator_phone, creator_country, creator_photo, creator_bio) VALUES (?, ?, ?, ?, ?, ?, ?, ?);',
-            [req.body.name, req.body.pass, req.body.nick, req.body.email, req.body.phone, req.body.country, req.body.photo, req.body.bio],
-            (error, resultado, field) =>{
-                conn.release();
-            }
-        )
-    });
-
-
-
 
     const pedido = {
         id_pedido: req.body.id_pedido,
@@ -39,20 +23,19 @@ router.post('/', (req, res, next)=> {
     }
 
     res.status(201).send({
-        mensagem: 'Usando o POST dentro da rota de pedidos',
+        mensagem: 'Usando o POST dentro da rota de criadores',
         pedido: pedido
     });
 });
 
 
-//Retorna um pedido em especifíco
-router.get('/:id_pedido',  (req, res, next)=>{
-    const id = req.params.id_pedido;
+router.get('/:creatorname',  (req, res, next)=>{
+    const id = req.params.creatorname;
 
     if (id == 1){
         res.status(200).send({
-            mensagem: 'Você descobriu um ID especial',
-            id: id
+            mensagem: 'Creatorname encontrado',
+            creatorname: creatorname
         });
     } else {
         res.status(200).send({
@@ -63,33 +46,10 @@ router.get('/:id_pedido',  (req, res, next)=>{
 });
 
 
-
 router.delete('/', (req, res, next)=> {
     res.status(201).send({
-        mensagem: 'Usando o DELETE dentro da rota de pedidos'
+        mensagem: 'Usando o DELETE dentro da rota de criadores'
     });
 });
-
-
-async function inserirDados() {
-    const pool = createConnection(); // Cria uma nova conexão
-  
-    try {
-      // Query de inserção
-      const queryString = 'INSERT INTO sua_tabela (coluna1, coluna2) VALUES ($1, $2)';
-      const values = ['valor1', 'valor2']; // Valores que serão inseridos nas colunas
-  
-      // Executa a query de inserção
-      const result = await pool.query(queryString, values);
-  
-      console.log('Inserção realizada com sucesso!');
-      console.log('Linhas afetadas:', result.rowCount);
-    } catch (err) {
-      console.error('Erro ao inserir dados:', err);
-    } finally {
-      // Fecha a conexão após a inserção
-      pool.end();
-    }
-  }
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,31 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 
-//Return all products 
 router.get('/', (req, res, next)=> {
     res.status(200).send({
-        mensagem: 'Usando o GET dentro da rota de produtos'
+        mensagem: 'Usando o GET dentro da rota de usuários'
     });
 });
 
-//Insert a product 
+
 router.post('/', (req, res, next)=> {
-    const produto ={
-        id_produto: req.body.id_produto,
-        quantidade: req.body.quantidade
+   
+    const user ={
+        username: req.body.username,
+        name: req.body.name
     }
 
     
     res.status(201).send({
         mensagem: 'Usando o POST dentro da rota de produtos',
-        produtoCriado: produto
+        createdUser: user
     });
 });
 
 
-//Return a specific product
-router.get('/:id_produto',  (req, res, next)=>{
-    const id = req.params.id_produto;
+
+router.get('/:username',  (req, res, next)=>{
+    const id = req.params.username;
 
     if (id == 1){
         res.status(200).send({
@@ -41,16 +41,17 @@ router.get('/:id_produto',  (req, res, next)=>{
     }
 });
 
+
 router.patch('/', (req, res, next)=> {
     res.status(201).send({
-        mensagem: 'Usando o PATCH dentro da rota de produtos'
+        mensagem: 'Usando o PATCH dentro da rota de usuários'
     });
 });
 
 
 router.delete('/', (req, res, next)=> {
     res.status(201).send({
-        mensagem: 'Usando o DELETE dentro da rota de produtos'
+        mensagem: 'Usando o DELETE dentro da rota de usuários'
     });
 });
 
